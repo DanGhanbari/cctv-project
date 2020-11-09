f379e93e7b518997fbbd441d8371630b66bbcb13
daniel <danielghanbari7@gmail.com>
2020-11-09 22:06:19 +0000

    itemlink page created

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,9 @@
+// if (process.env.NODE_ENV !== "production") {
+//   require("dotenv").load();
+// }
+// const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+// console.log(stripeSecretKey);
+
 import express from "express";
 import data from "./data";
 
@@ -10,3 +16,12 @@ app.get("/", (req, res) => {
 app.listen(5000, () => {
   console.log("server started at http://localhost:5000");
 });
+
+
+// const express = require("express");
+// const app = express();
+
+// app.set("view engine", "ejs");
+// app.use(express.static("public"));
+
+// app.listen(5000, console.log("connected"));
