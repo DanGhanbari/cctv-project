fc416ad3fc8f5f19d5421342fb3e8d77f1f470b4
daniel <danielghanbari7@gmail.com>
2020-11-09 09:12:54 +0000

    frontend and backend

--- backend/data.js
+++ backend/data.js
@@ -1,64 +1,66 @@
 export default {
-  items: {
-    itemsCamera: [
-      {
-        name: "camera1",
-        image: "#",
-        price: 120,
-        brand: "HikVision",
-        availibity: "Out of Stock",
-      },
-      {
-        name: "camera2",
-        image: "#",
-        price: 150,
-        brand: "HikVision",
-        availibity: "Out of Stock",
-      },
-      {
-        name: "camera3",
-        image: "#",
-        price: 180,
-        brand: "HikVision",
-        availibity: "Out of Stock",
-      },
-      {
-        name: "camera4",
-        image: "#",
-        price: 200,
-        brand: "HikVision",
-        availibity: "Out of Stock",
-      },
-    ],
-    itemsDVR: [
-      {
-        name: "DVR1",
-        image: "#",
-        price: 120,
-        brand: "HikVision",
-        availibity: "Out of Stock",
-      },
-      {
-        name: "DVR2",
-        image: "#",
-        price: 150,
-        brand: "HikVision",
-        availibity: "Out of Stock",
-      },
-      {
-        name: "DVR3",
-        image: "#",
-        price: 180,
-        brand: "HikVision",
-        availibity: "Out of Stock",
-      },
-      {
-        name: "DVR4",
-        image: "#",
-        price: 200,
-        brand: "HikVision",
-        availibity: "Out of Stock",
-      },
-    ],
-  },
+  items: [
+    {
+      itemCamera: [
+        {
+          name: "camera1",
+          image: "#",
+          price: 120,
+          brand: "HikVision",
+          availibity: "Out of Stock",
+        },
+        {
+          name: "camera2",
+          image: "#",
+          price: 150,
+          brand: "HikVision",
+          availibity: "Out of Stock",
+        },
+        {
+          name: "camera3",
+          image: "#",
+          price: 180,
+          brand: "HikVision",
+          availibity: "Out of Stock",
+        },
+        {
+          name: "camera4",
+          image: "#",
+          price: 200,
+          brand: "HikVision",
+          availibity: "Out of Stock",
+        },
+      ],
+      itemDVR: [
+        {
+          name: "DVR1",
+          image: "#",
+          price: 120,
+          brand: "HikVision",
+          availibity: "Out of Stock",
+        },
+        {
+          name: "DVR2",
+          image: "#",
+          price: 150,
+          brand: "HikVision",
+          availibity: "Out of Stock",
+        },
+        {
+          name: "DVR3",
+          image: "#",
+          price: 180,
+          brand: "HikVision",
+          availibity: "Out of Stock",
+        },
+        {
+          name: "DVR4",
+          image: "#",
+          price: 200,
+          brand: "HikVision",
+          availibity: "Out of Stock",
+        },
+      ],
+    },
+  ],
 };
--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,6 @@ app.get("/", (req, res) => {
   res.send(data.items);
 });
 
-// app.use(express.static("myapp"));
-
 app.listen(5000, () => {
   console.log("server started at http://localhost:5000");
 });
-
-// const express = require("express");
-// const app = express();
-// app.listen(5000, () => console.log("listening at 5000"));
